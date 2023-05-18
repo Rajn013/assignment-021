f2efcbc6123d0fe8cb59b00954eda01b9b63269e
Rajandep singh matharu <105154536+Rajn013@users.noreply.github.com>
2023-05-18 16:11:00 +0530

    Add files via upload

---
+++ Untitled84.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is a probability distribution, exactly? If the values are meant to be random, how can you predict them at all?
+# 
+
+# A probability distribution tells us how likely different outcomes are in a random event. While we can't predict specific values, the distribution helps us understand the overall chances and patterns of those values. It allows us to make predictions based on probabilities and estimate the possible range of outcomes.
+
+# Q2. Is there a distinction between true random numbers and pseudo-random numbers, if there is one? Why are the latter considered “good enough”?
+# 
+
+# True random numbers are genuinely unpredictable, while pseudo-random numbers are generated using algorithms and appear random but are actually predictable. Pseudo-random numbers are considered "good enough" for most purposes because they exhibit statistical randomness, are efficient, and widely applicable.
+
+# Q3. What are the two main factors that influence the behaviour of a "normal" probability distribution?
+# 
+
+#  the average (mean) and the spread (standard deviation). The mean determines the center of the distribution, while the standard deviation determines how spread out the values are around the mean.
+
+# Q4. Provide a real-life example of a normal distribution.
+# 
+
+# 
+# Human heights follow a normal distribution. Most people have heights close to the average, with fewer individuals being very tall or very short. 
+
+# Q5. In the short term, how can you expect a probability distribution to behave? What do you think will happen as the number of trials grows?
+# 
+
+# A probability distribution can behave unpredictably. However, as the number of trials increases, the distribution becomes more predictable and aligns with its expected shape and probabilities.
+
+# Q6. What kind of object can be shuffled by using random.shuffle?
+# 
+
+# The random.shuffle function can shuffle the elements of a list or any other iterable sequence.
+
+# Q7. Describe the math package's general categories of functions.
+# 
+
+# Basic arithmetic operations (addition, subtraction, multiplication, division)
+# Trigonometric calculations (sine, cosine, tangent)
+# Exponential and logarithmic operations
+# Hyperbolic trigonometry
+# Rounding and precision manipulation
+# Special mathematical functions (factorial, absolute value, square root, etc.)
+
+# Q8. What is the relationship between exponentiation and logarithms?
+# 
+
+# 
+# Exponentiation calculates the result of multiplying a base by itself for a certain number of times. Logarithms help us find the exponent needed to obtain a specific value when a base is raised to that exponent. 
+
+# Q9. What are the three logarithmic functions that Python supports?
+# 
+
+# math.log(x): Computes the natural logarithm (base e) of a number x.
+# 
+# math.log10(x): Calculates the base-10 logarithm of a number x.
+# 
+# math.log(x, base): Computes the logarithm of x with a specified base.
+
+# In[ ]:
+
+
+
+
